9249a7c1d0f559288e24391b068a369c3f8d19c7
mnguyen0226 <mnguyen0226@vt.edu>
2023-04-26 13:29:07 -0400

    push new boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Capturing Engagement As A Multidimensional Datapoint
+
+- References
+  - https://www.youtube.com/watch?v=tPnX0HNFx_0&ab_channel=TylerPotts
+  - https://github.com/tylerPottsDev/yt-vuejs-infinite-scroll
+- Project run with Vue CLI
+  - npm install -g @vue/cli
+  - vue create .
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,107 @@
-<script setup lang="ts">
-import { provide } from "vue";
-import { RouterView, useRoute } from "vue-router";
-import type { CategoryItem } from "@/types";
-import TheHeader from "@/components/TheHeader.vue";
-import TheFooter from "@/components/TheFooter.vue";
-import sourceData from "@/db.json";
-const route = useRoute();
-const categoryList = sourceData.categories as CategoryItem[];
-provide("categoryList", categoryList);
+<template>
+	<div>
+		<header>
+			<h1>My Movie Feed</h1>
+		</header>
+		<main>
+      <!-- Create articles -->
+			<Post v-for="(anime, i) in anime_list" :key="i" :anime="anime" />
+		</main>
+	</div>
+</template>
+
+<script>
+// import infinite scroll
+import Post from "./components/MyPost.vue";
+
+export default {
+	name: "App",
+	data() {
+		return {
+			anime_list: [],
+		};
+	},
+	components: {
+		Post,
+	},
+	methods: {
+    // anime
+		getAnime() {
+			const anime_titles = [
+				"Naruto",
+				"Demon Slayer",
+				"Dragon Ball",
+				"My Hero Academia",
+				"Sword Art Online",
+				"Tokyo Ghoul",
+				"Darling in the Franxx",
+				"Code Geass",
+				"One Piece",
+				"Fairy Tail",
+				"Bleach",
+				"Attack on Titan",
+				"Hunter x Hunter",
+			];
+
+			const anime = [];
+
+			for (let i = 0; i < 10; i++) {
+				anime.push({
+					title: anime_titles[
+						Math.floor(Math.random() * anime_titles.length)
+					],
+					description:
+						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+				});
+			}
+
+			return anime;
+		},
+		handleScroll() {
+			if (
+				window.scrollY + window.innerHeight >=
+				document.body.scrollHeight - 50
+			) {
+				const new_anime = this.getAnime();
+
+				this.anime_list = [...this.anime_list, ...new_anime];
+			}
+		},
+	},
+	mounted() {
+		this.anime_list = this.getAnime();
+		window.addEventListener("scroll", this.handleScroll);
+	},
+};
 </script>
 
-<template>
-  <div id="app">
-    <the-header></the-header>
-    <router-view :key="route.fullPath"></router-view>
-    <the-footer></the-footer>
-  </div>
-</template>
+<style>
+* {
+	margin: 0;
+	box-sizing: border-box;
+}
+
+body {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #fff;
+	background-color: #2c3e50;
+	min-height: 100vh;
+	padding-top: 3rem;
+}
+
+header h1 {
+	text-align: center;
+}
+
+header {
+	margin-bottom: 2rem;
+}
+
+main {
+	padding: 0 2rem;
+	max-width: 640px;
+	margin: 0 auto;
+}
+</style>
