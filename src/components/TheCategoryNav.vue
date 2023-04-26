eef5358558762d43300f461f508752058fe455bb
mnguyen0226 <mnguyen0226@vt.edu>
2023-04-26 12:15:02 -0400

    upload front-end boilerplate

---
+++ src/components/TheCategoryNav.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { inject } from "vue";
+import type { CategoryItem } from "@/types";
+const categoryList = inject("categoryList") as CategoryItem[];
+</script>
+
+<style scoped>
+.category-buttons {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  text-align: center;
+  justify-content: space-between;
+}
+
+.button.category-button {
+  background-color: white;
+  color: var(--secondary-background-color);
+}
+
+.button.category-button.router-link-active,
+.button.category-button:hover,
+.button.category-button:active {
+  background-color: black;
+  color: var(--secondary-text-color);
+}
+
+
+.blue-border-nav-bar {
+  border-top-style: solid;
+  border-bottom-style: solid;
+  border-width: thin;
+  border-color: #00239E;
+  background: white;
+  color: #00239E;
+}
+</style>
+
+<template>
+  <nav class="category-nav blue-border-nav-bar">
+    <ul class="category-buttons">
+      <li v-for="category in categoryList" :key="category.categoryId">
+        <router-link
+          :to="'/category/' + category.name"
+          class="button category-button"
+        >
+          {{ category.name }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ src/components/TheHeaderDropdown.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+import { inject } from "vue";
+import type { CategoryItem } from "@/types";
+const categoryList = inject("categoryList") as CategoryItem[];
+</script>
+
+<style scoped>
+.header-dropdown {
+  position: relative;
+}
+
+.categories-button {
+  background-color: var(--primary-color-dark);
+  margin-top: 0.25em;
+}
+
+.header-dropdown ul {
+  background-color: white;
+  color: black;
+  display: none;
+}
+
+.header-dropdown li {
+  padding: 0.25em 0.5em;
+}
+
+.header-dropdown a {
+  color: #00239E;
+  background: white;
+  text-decoration: none;
+}
+
+.header-dropdown a:hover {
+  background: var(--primary-color-dark);
+  color: white;
+}
+
+/* self-implemented*/
+.header-dropdown li:hover a {
+  background: var(--primary-color-dark);
+  color: white;
+}
+
+.header-dropdown li:hover {
+  background: var(--primary-color-dark);
+  color: white;
+  border-radius: 20px;
+  border-style: solid;
+  border-width: thin;
+  border-color: #00239E;
+}
+
+.header-dropdown:hover ul {
+  display: block;
+  position: absolute;
+  min-width: 8em;
+  z-index: 1;
+  cursor: pointer;
+}
+
+/* self-implemented */
+.categories-button-style{
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
+  padding-right: 1em;
+  padding-left: 1em;
+  border-radius: 20px;
+}
+
+/*should be in header*/
+.categories-element {
+  border-radius: 20px;
+  border-style: solid;
+  border-width: thin;
+
+  border-color: #00239E;
+  /* color: #00239E; */
+}
+</style>
+
+<template>
+  <div class="header-dropdown">
+    <button class="button categories-button-style blue-border-white-bg">
+      Categories
+      <i class="fa fa-caret-down"></i>
+    </button>
+
+    <ul class="categories-element">
+      <li v-for="category in categoryList" :key="category.categoryId">
+        <router-link :to="'/category/' + category.name">
+          {{ category.name }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
