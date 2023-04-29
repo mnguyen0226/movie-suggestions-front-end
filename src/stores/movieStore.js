0d6935222101b6fd0b004a78a7e18cb386ef4d8e
mnguyen0226 <mnguyen0226@vt.edu>
2023-04-29 15:06:02 -0400

    clean up code

--- src/stores/movieStore.js
+++ src/stores/movieStore.js
@@ -11,17 +11,13 @@ export const useMovieStore = defineStore("CartStore", {
     },
     actions: {
         async fetchMovies() {
-            console.log("Called here")
             const someList =await fetch("http://localhost:3000/home-page").then((response)=>response.json())
 
             for(const movie in someList){
                 this.movie_list.push(someList[movie])
             }
             return this.movie_list;
-
-        //   this.movie_list = movies;
         },
     }
-    //write an action t
   });
   
